d05acdc49fe65da5117f510d7d47647e440e1936
Alex Haristoy <alexandre.haristoy@gmail.com>
2021-12-05 14:45:44 +0100

    Add : add transaction manually

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ TRADING = 11 # SELL
 STATUS = 12
 ORDER_NUM = 2
 
+transactions_list = []
 
 def reading_input_file_and_return_transactions_list(input_file):
     input_file = openpyxl.load_workbook(input_file, read_only=True)
@@ -68,47 +69,82 @@ def add_transaction_in_its_currency(transaction_list, currency_list):
                 """print("Trading add in ", currency.name, " : ", transaction.side, " ", transaction.quantityBuy, " ", transaction.currencyBuy, " for ", transaction.quantitySell, " ",
                       transaction.currencySell)"""
 
+def add_new_transaction_manually():
+    print("Add new transaction :")
+    print("Currency buy :")
+    currency_buy = input()
+    print("Quantity buy :")
+    quantity_buy = float(input())
+    print("Currency sell :")
+    currency_sell = input()
+    print("Quantity sell :")
+    quantity_sell = float(input())
+    print("Side (BUY or SELL) :")
+    side = input()
+    print("Date (year-month-day xx:xx:xx) :")
+    date = input()
+    print("Number order:")
+    order_num = input()
+    return Transactions(currency_buy, quantity_buy, currency_sell, quantity_sell, side, date, order_num)
 
-def option0(currencies_list):
+
+def option0(adding_type):
+    # Add transaction list from an input file"
+    if adding_type == "0":
+        list_from_input_file = reading_input_file_and_return_transactions_list("Binance1116.xlsx")
+        for transaction in list_from_input_file:
+            transactions_list.append(transaction)
+    # Add transaction manually
+    elif adding_type == "1":
+        transactions_list.append(add_new_transaction_manually())
+
+    for t in transactions_list:
+        print(t.transactionNumber)
+    # sort by date the list of transactions0
+    sorted(transactions_list, key=lambda transaction: transaction.date)
+    # Create the list of currencies
+    currencies_list = creation_of_trade_currency_list(transactions_list)
+    # Add each transaction to his currency
+    add_transaction_in_its_currency(transactions_list, currencies_list)
+    # Calculation of transactions for all currencies
+    for currency in currencies_list:
+        currency.transactions_overview()
+
+    return currencies_list
+
+def option1(currencies_list):
     print(" List of currencies: ")
     for currency in currencies_list:
         print(currency.name)
 
 
-def option1(currency_choose, currencies_list):
+def option2(currency_choose, currencies_list):
     for currency in currencies_list:
         if currency.name == currency_choose:
             currency.print_overview()
 
 
-def option2():
-    print("option 2")
-
-
 def main():
-    transactions_list = reading_input_file_and_return_transactions_list("Binance1116.xlsx")
-    currencies_list = creation_of_trade_currency_list(transactions_list)
-    add_transaction_in_its_currency(transactions_list, currencies_list)
-
-    # Calculation of transactions for all currencies
-    for currency in currencies_list:
-        currency.transactions_overview()
 
     while True:
         print("CHOOSE AN OPTION ")
-        print("0 : List of currencies")
-        print("1 : Currency overview")
+        print("0 : Add transaction table")
+        print("1 : Print list of currencies")
+        print("2 : Currency overview")
         x = input()
         if x == "0":
-            option0(currencies_list)
+            print("0 : Add transaction list from an input file")
+            print("1 : Add transaction manually")
+            adding_type = input()
+            currencies_list = option0(adding_type)
+
         elif x == "1":
+            option1(currencies_list)
+
+        elif x == "2":
             print("What currency would you like to see ?")
             currency_choose = input()
-            option1(currency_choose, currencies_list)
-        elif x == "2":
-            option2()
-        elif x == "3":
-            break
+            option2(currency_choose, currencies_list)
 
 
 main()
